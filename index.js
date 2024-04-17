0423d9da8d56e9566a496feac7548e8f95aa1101
Samuel Kirori <samuelkirori300@gmail.com>
2024-04-17 16:59:51 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  word = word.toLowerCase();
+
+  //initialize first and last characters of the word
+  let start = 0;
+  let end = word.length -1;
+
+  while(start <= end){
+    if(word[start] !== word[end]){
+      return false;
+    }
+    start++;
+    end--;
+  }
+return true;
+
 }
 
 /* 
@@ -21,5 +36,30 @@ if (require.main === module) {
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
+console.log("Expecting:true");
+  console.log("=>", isPalindrome("abba"));
+
+    console.log("");
+
+  console.log("Expecting:true");
+  console.log("=>", isPalindrome("racecar"));
+  
+  console.log("");
+
+  console.log("Expecting:true");
+  console.log("=>", isPalindrome("a")); 
+  
+  console.log("");
+
+  console.log("Expecting:false");
+  console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+
+  console.log("Expecting:false");
+  console.log("=>", isPalindrome("ab"));
+
+
 
 module.exports = isPalindrome;
